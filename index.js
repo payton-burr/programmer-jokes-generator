01dcf0b54d2e23486a43212e33630406b5725b83
Payton <greatpayton@gmail.com>
2020-10-11 20:25:36 -0600

    declared global selectors

---
+++ index.js
@@ -0,0 +1,4 @@
+const setupDiv = document.querySelector('#setup');
+const punchlineDiv = document.querySelector('#punchline');
+const punchlineBtn = document.querySelector('.punchlineBtn');
+const newJokeBtn = document.querySelector('.newJokeBtn');
