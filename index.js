bb2e390c7b30c947ea358c8337f46a82444ea741
Payton <greatpayton@gmail.com>
2020-10-11 20:38:09 -0600

    joke setup is now inserted into setupDiv

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const getJoke = async () => {
     'https://official-joke-api.appspot.com/jokes/programming/random'
   );
   const joke = await jokePromise.json();
-
-  console.log(joke);
+  setupDiv.textContent = joke[0].setup;
 };
 
 getJoke();
